e8685a765e00687b018cb6153816d3221e0dc583
roudabehadnani <rodabeh.adnani@gmail.com>
2022-09-14 20:39:20 +0200

    Initial commit

---
+++ src/main/java/com/example/jpa_workshop_bookloan/JpaWorkshopBookloanApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_workshop_bookloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshopBookloanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWorkshopBookloanApplication.class, args);
+    }
+
+}
